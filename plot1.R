c716a5645717bca07bbd4fd2514b278a0f71013d
Bartel de Leeuw <barteldeleeuw@gmail.com>
2014-05-09 23:32:42 -0400

    First version of Plots

---
+++ plot1.R
@@ -0,0 +1,33 @@
+# read data from file
+data <- read.csv("data\\household_power_consumption.txt",
+                 sep=";",
+                 na.strings="?",
+                 colClasses=c("character","character","numeric","numeric",
+                              "numeric","numeric","numeric","numeric","numeric"),
+                 comment.char="")
+
+# convert date and time fields to a DateTime field
+data$DateTime = strptime(paste(data$Date,data$Time), 
+                         "%d/%m/%Y %H:%M:%S")
+data <- data[,-c(1,2)]
+
+#filter for dates 
+filteredData <- subset(data, 
+                       DateTime >= as.POSIXct('2007-02-01') & 
+                         DateTime < as.POSIXct('2007-02-03'))
+
+# create a 480x480 png, 
+# with transparent background
+png(filename="plot1.png",
+    width=480,
+    height=480,
+    bg="transparent")
+
+#create the histogram in red
+hist(filteredData$Global_active_power,
+     col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+# close the device
+dev.off()
